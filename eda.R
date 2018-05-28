6698c088da423fd4849612a3b218fbbce7d58f2f
Wise Chirag <chiragrawal@protonmail.com>
2018-05-28 13:11:52 +0530

    Creating seperate files for EDA and Global (variable, packages and functions)

---
+++ eda.R
@@ -0,0 +1,4 @@
+# Extensive EDA Script - In this script, I would just try to perform extensive EDA of 
+#                         data sets
+
+
---
+++ global.R
@@ -0,0 +1,22 @@
+
+# This is the global file of the project and shall have the following:
+  # 1) Data sets loaded from the source files
+  # 2) All package installations
+  # 3) All Functions
+
+
+#Installing the required packages
+list.of.packages<-c("randomForest","RCurl","jsonlite")
+new.packages<-list.of.packages[!(list.of.packages %in% install.packages()[,"Package"])]
+if(length(new.packages)) installed.packages(new.packages)
+
+
+#Reading the data
+application_train<-read.csv("./data/application_train.csv")
+POS_CASH_balance<-read.csv("./data/POS_CASH_balance.csv")
+bureau_balance<-read.csv("./data/bureau_balance.csv")
+previoius_application<-read.csv("./data/previous_application.csv")
+installment_payments<-read.csv("./data/installments_payments.csv")
+credit_card_balance<-read.csv("./data/credit_card_balance.csv")
+bureau<-read.csv("./data/bureau.csv")
+application_test<-read.csv("./data/application_test.csv")
